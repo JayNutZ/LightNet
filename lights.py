243b3edf004ffe415bcb30728d8fb066241c71b2
Jakob <jaaynutz@gmail.com>
2019-08-21 21:40:00 +0200

    Update lights.py

--- lights.py
+++ lights.py
@@ -1,29 +1,21 @@
 from color import Color
-from gpio import Gpio
+from controller import Controller
 
 
 class Lights:
-    gpio = Gpio()
-    on = False
+    gpio = Controller()
     pins = {'red': 1, 'green': 2, 'blue': 3}
     current_color = Color(0, 0, 0)
 
-    def __init__(self, pin_red, pin_green, pin_blue):
-        self.on = False
-        self.pins['red'] = pin_red
-        self.pins['green'] = pin_green
-        self.pins['blue'] = pin_blue
+    def __init__(self, pins):
+        self.pins = pins
 
     def stop(self):
         self.gpio.kill()
-        return self.on
 
     def colorize(self, color):
         self.current_color = color
         return self.gpio.update(self.pins, self.current_color)
-
-    def is_active(self):
-        return self.on
 
     def get_pins(self):
         return self.pins
